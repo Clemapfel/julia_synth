0683ebcc8328c09396eb446874f1149abe9fbfaa
Clemapfel <mail@clemens-cords.com>
2022-06-25 22:40:10 +0200

    hello world working

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <cmath>
+#include <cassert>
 
 #include <SFML/Audio.hpp>
 #include <SFML/System.hpp>
@@ -14,34 +15,37 @@
 #include <thread>
 
 #include <iostream>
-
-#include <Telescope/telescope.hpp>
+#include <cmath>
 
 int main()
 {
+    const size_t sample_rate = 44100;
 
-    only do adio initialize
-    ts::initialize();
-    auto window = ts::Window();
-    window.create(0, 0);
+    size_t n = sample_rate * 10;
+    std::vector<int16_t> samples;
+    for (size_t i = 0; i < n; ++i)
+        samples.push_back(sin(i / M_PI) * pow(2, 16) / 2);
 
+    auto chunk = sf::SoundBuffer();
+    chunk.loadFromSamples(samples.data(), samples.size(), 1, sample_rate);
 
-    while (window.is_open())
+    auto sound = sf::Sound();
+
+    auto chunk2 = sf::SoundBuffer();
+    chunk2.loadFromFile("/home/clem/Workspace/Telescope/test/ok_desu_ka.wav");
+
+    auto chunk1 = sf::SoundBuffer();
+    chunk1.loadFromSamples(samples.data(), samples.size(), 1, sample_rate);
+    sound.setBuffer(chunk1);
+    sound.play();
+
+    for (size_t i = 0; i < chunk1.getSampleCount(); ++i)
     {
-        auto time = ts::start_frame(&window);
+        auto val = chunk1.getSamples()[i];
 
-        if (ts::InputHandler::was_pressed(ts::SPACE))
-        {
-            std::cout << "start" << std::endl;
-        }
-
-        if (ts::InputHandler::was_released(ts::SPACE))
-        {
-            std::cout << "stop" << std::endl;
-        }
-
-        ts::end_frame(&window);
+        if (abs(val) > 16)
+            std::cout << val << std::endl;
     }
 
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::seconds(3));
 }
